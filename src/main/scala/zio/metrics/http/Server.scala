4fb8512b45cce1175bdd57f948d080293e1ed439
toxicafunk <toxicafunk@gmail.com>
2019-08-18 22:38:33 +0200

    Update to ZIO RC11-1 (#7)

--- src/main/scala/zio/metrics/http/Server.scala
+++ src/main/scala/zio/metrics/http/Server.scala
@@ -4,7 +4,7 @@ import cats.data.Kleisli
 import org.http4s.server.blaze._
 import org.http4s.{ Request, Response }
 import zio.metrics.Metrics
-import zio.{ Task, TaskR, ZIO }
+import zio.{ RIO, Task, ZIO }
 //import zio.scheduler.Scheduler
 import zio.interop.catz._
 import scala.util.Properties.envOrNone
@@ -18,7 +18,7 @@ object Server {
   val port: Int = envOrNone("HTTP_PORT").fold(9090)(_.toInt)
 
   type HttpEnvironment = Clock with Console with System with Random with Blocking
-  type HttpTask[A]     = TaskR[HttpEnvironment, A]
+  type HttpTask[A]     = RIO[HttpEnvironment, A]
 
   type KleisliApp = Kleisli[HttpTask, Request[HttpTask], Response[HttpTask]]
 
