b85ec7771eb16bdb6add33eb99793c461430bba0
toxicafunk <toxicafunk@gmail.com>
2020-04-15 11:51:51 +0200

    Added explicit layer that allows using existing s

--- prometheus/src/main/scala/zio/metrics/prometheus/package.scala
+++ prometheus/src/main/scala/zio/metrics/prometheus/package.scala
@@ -1,6 +1,6 @@
 package zio.metrics
 
-import zio.{ Has, Layer, ZLayer }
+import zio.{ Has, ZLayer }
 import zio.{ Ref, Task, UIO }
 
 package object prometheus {
@@ -26,65 +26,74 @@ package object prometheus {
     type Percentile = Double
     type Tolerance  = Double
 
-    val live: Layer[Nothing, Registry] = ZLayer.succeed(new Service {
-      private val registryRef: UIO[Ref[CollectorRegistry]] = Ref.make(CollectorRegistry.defaultRegistry)
+    val explicit: ZLayer[Has[Option[CollectorRegistry]], Nothing, Has[Registry.Service]] =
+      ZLayer.fromFunction[Has[Option[CollectorRegistry]], Registry.Service](
+        optionalRegistry =>
+          new Service {
+            private val registryRef: UIO[Ref[CollectorRegistry]] = {
+              val registry = optionalRegistry.get
+              Ref.make(registry.getOrElse(CollectorRegistry.defaultRegistry))
+            }
 
-      def getCurrent(): UIO[CollectorRegistry] = registryRef >>= (_.get)
+            def getCurrent(): UIO[CollectorRegistry] = registryRef >>= (_.get)
 
-      def registerCounter[A: Show](label: Label[A]): Task[PCounter] =
-        registryRef >>= (_.modify(r => {
-          val name = Show[A].show(label.name)
-          val c = PCounter
-            .build()
-            .name(name)
-            .labelNames(label.labels: _*)
-            .help(s"$name counter")
-            .register(r)
-          (c, r)
-        }))
+            def registerCounter[A: Show](label: Label[A]): Task[PCounter] =
+              registryRef >>= (_.modify(r => {
+                val name = Show[A].show(label.name)
+                val c = PCounter
+                  .build()
+                  .name(name)
+                  .labelNames(label.labels: _*)
+                  .help(s"$name counter")
+                  .register(r)
+                (c, r)
+              }))
 
-      def registerGauge[L: Show](label: Label[L]): Task[PGauge] =
-        registryRef >>= (_.modify(r => {
-          val name = Show[L].show(label.name)
-          val g = PGauge
-            .build()
-            .name(name)
-            .labelNames(label.labels: _*)
-            .help(s"$name gauge")
-            .register(r)
-          (g, r)
-        }))
+            def registerGauge[L: Show](label: Label[L]): Task[PGauge] =
+              registryRef >>= (_.modify(r => {
+                val name = Show[L].show(label.name)
+                val g = PGauge
+                  .build()
+                  .name(name)
+                  .labelNames(label.labels: _*)
+                  .help(s"$name gauge")
+                  .register(r)
+                (g, r)
+              }))
 
-      def registerHistogram[L: Show](label: Label[L], buckets: Buckets): Task[PHistogram] =
-        registryRef >>= (_.modify(r => {
-          val name = Show[L].show(label.name)
-          val hb = PHistogram
-            .build()
-            .name(name)
-            .labelNames(label.labels: _*)
-            .help(s"$name histogram")
+            def registerHistogram[L: Show](label: Label[L], buckets: Buckets): Task[PHistogram] =
+              registryRef >>= (_.modify(r => {
+                val name = Show[L].show(label.name)
+                val hb = PHistogram
+                  .build()
+                  .name(name)
+                  .labelNames(label.labels: _*)
+                  .help(s"$name histogram")
 
-          val h = buckets match {
-            case DefaultBuckets(bs)          => if (bs.isEmpty) hb else hb.buckets(bs: _*)
-            case LinearBuckets(s, w, c)      => hb.linearBuckets(s, w, c)
-            case ExponentialBuckets(s, f, c) => hb.exponentialBuckets(s, f, c)
+                val h = buckets match {
+                  case DefaultBuckets(bs)          => if (bs.isEmpty) hb else hb.buckets(bs: _*)
+                  case LinearBuckets(s, w, c)      => hb.linearBuckets(s, w, c)
+                  case ExponentialBuckets(s, f, c) => hb.exponentialBuckets(s, f, c)
+                }
+                (h.register(r), r)
+              }))
+
+            def registerSummary[L: Show](label: Label[L], quantiles: List[(Percentile, Tolerance)]): Task[PSummary] =
+              registryRef >>= (_.modify(r => {
+                val name = Show[L].show(label.name)
+                val sb = PSummary
+                  .build()
+                  .name(name)
+                  .labelNames(label.labels: _*)
+                  .help(s"$name timer")
+
+                val s = quantiles.foldLeft(sb)((acc, c) => acc.quantile(c._1, c._2)).register(r)
+                (s, r)
+              }))
           }
-          (h.register(r), r)
-        }))
+      )
 
-      def registerSummary[L: Show](label: Label[L], quantiles: List[(Percentile, Tolerance)]): Task[PSummary] =
-        registryRef >>= (_.modify(r => {
-          val name = Show[L].show(label.name)
-          val sb = PSummary
-            .build()
-            .name(name)
-            .labelNames(label.labels: _*)
-            .help(s"$name timer")
-
-          val s = quantiles.foldLeft(sb)((acc, c) => acc.quantile(c._1, c._2)).register(r)
-          (s, r)
-        }))
-    })
+    val live: ZLayer[Any, Nothing, Has[Registry.Service]] = ZLayer.succeed[Option[CollectorRegistry]](None) >>> explicit
 
   }
 }
