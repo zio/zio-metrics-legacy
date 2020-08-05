bf411595aeed12b19b03089dcbc04efea8d59b2d
Sebastian Vercruysse <sebastian.vercruysse@gmail.com>
2020-08-05 13:34:49 +0200

    Fixes tests

--- statsd/src/test/scala/zio/metrics/ClientTest.scala
+++ statsd/src/test/scala/zio/metrics/ClientTest.scala
@@ -3,23 +3,23 @@ package zio.metrics
 import zio.clock.Clock
 import zio.console._
 import zio.metrics.encoders._
-import zio.{ RIO, Runtime, Task }
+import zio.{ Chunk, RIO, Runtime, Task }
 
 object ClientTest {
 
   val rt = Runtime.unsafeFromLayer(Encoder.statsd ++ Console.live ++ Clock.live)
 
-  val myudp: List[Metric] => RIO[Encoder with Console, List[Int]] = msgs =>
+  val myudp: Chunk[Metric] => RIO[Encoder with Console, Chunk[Int]] = msgs =>
     for {
       sde <- RIO.environment[Encoder]
       opt <- RIO.foreach(msgs)(sde.get.encode(_))
       _   <- putStrLn(s"udp: $opt")
-      l   <- RIO.foreach(opt.flatten)(s => UDPClient().use(_.send(s)))
+      l   <- RIO.foreach(opt.collect { case Some(msg) => msg })(s => UDPClient().use(_.send(s)))
     } yield l
 
   val program = {
     val messages = List(1.0, 2.2, 3.4, 4.6, 5.1, 6.0, 7.9)
-    val createClient = Client.withListener[List, Int] { l: List[Metric] =>
+    val createClient = Client.withListener[Chunk, Int] { l: Chunk[Metric] =>
       myudp(l).provideSomeLayer[Encoder](Console.live)
     }
     createClient.use { client =>
