26e6506be46092d43b05de1ebcaa6dcd2724d2ac
toxicafunk <toxicafunk@gmail.com>
2021-04-20 00:29:32 +0200

    Use async send for counter

--- statsd/src/test/scala/zio/metrics/StatsDClientTest.scala
+++ statsd/src/test/scala/zio/metrics/StatsDClientTest.scala
@@ -1,14 +1,14 @@
 package zio.metrics
 
-import zio.ZIO
 import zio.clock.Clock
+import zio.console.putStrLn
 import zio.metrics.encoders._
 import zio.metrics.statsd._
 import zio.test._
 import zio.test.Assertion._
 import zio.duration._
 import zio.test.TestAspect.timeout
-import zio.ZManaged
+import zio.URIO
 
 object StatsDClientTest extends DefaultRunnableSpec {
   private val port = 8922
@@ -16,25 +16,29 @@ object StatsDClientTest extends DefaultRunnableSpec {
   override def spec =
     suite("StatsDClient")(
       testM("sends correct metrics via UDP") {
-        val clientWithAgent: ZManaged[Client.ClientEnv, Throwable, (StatsDClient, UDPAgent)] = for {
-          u <- UDPAgent(port)
-          d <- StatsDClient(500, 5000, 100, Some("localhost"), Some(port))
-        } yield (d, u)
+        val clientWithAgent = (
+          for {
+            u <- UDPAgent(port)
+            d <- StatsDClient(500, 5000, 100, Some("localhost"), Some(port))
+            _ <- putStrLn("Make is ready").toManaged(_ => URIO.unit)
+          } yield (d, u)
+        )
 
         clientWithAgent.use {
           case (client, agent) =>
             for {
-              _            <- ZIO.sleep(10.seconds)
-              _            <- client.increment("TestCounter", 0.9)
+              _            <- putStrLn("Starting use")
+              _            <- client.increment("TestCounter", 0.9, false)
               clientMetric <- agent.nextReceivedMetric
               _            <- client.timer("TestTimer", 0.44, 0.9)
               timerMetric  <- agent.nextReceivedMetric
+              _            <- putStrLn("Finished use")
             } yield {
               assert(clientMetric)(equalTo("TestCounter:1|c|@0.9")) &&
               assert(timerMetric)(equalTo("TestTimer:0.44|ms|@0.9"))
             }
         }
-      }
-    ).provideCustomLayer(Encoder.statsd ++ Clock.live) @@ timeout(180.seconds)
+      }.provideCustomLayer(Encoder.statsd ++ Clock.live) @@ timeout(300.seconds)
+    )
 
 }
