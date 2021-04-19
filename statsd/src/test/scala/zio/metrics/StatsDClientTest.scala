0670c90e43c997fc0fb9011f121b25c0c6052347
toxicafunk <toxicafunk@gmail.com>
2021-04-19 23:46:33 +0200

    Sleep before increment

--- statsd/src/test/scala/zio/metrics/StatsDClientTest.scala
+++ statsd/src/test/scala/zio/metrics/StatsDClientTest.scala
@@ -1,6 +1,6 @@
 package zio.metrics
 
-import zio.{ URIO, ZIO }
+import zio.ZIO
 import zio.clock.Clock
 import zio.metrics.encoders._
 import zio.metrics.statsd._
@@ -18,13 +18,13 @@ object StatsDClientTest extends DefaultRunnableSpec {
       testM("sends correct metrics via UDP") {
         val clientWithAgent: ZManaged[Client.ClientEnv, Throwable, (StatsDClient, UDPAgent)] = for {
           u <- UDPAgent(port)
-          _ <- ZIO.sleep(10.seconds).toManaged(_ => URIO.unit)
           d <- StatsDClient(500, 5000, 100, Some("localhost"), Some(port))
         } yield (d, u)
 
         clientWithAgent.use {
           case (client, agent) =>
             for {
+              _            <- ZIO.sleep(10.seconds)
               _            <- client.increment("TestCounter", 0.9)
               clientMetric <- agent.nextReceivedMetric
               _            <- client.timer("TestTimer", 0.44, 0.9)
